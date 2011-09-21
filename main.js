c0b863b5557c4b1fa1368e4a28c4796b24ddce03
Aaron Gloege <littledude835@gmail.com>
2011-09-20 20:06:09 -0500

    Remove mobile urle search and create Const variable for easily update/maintain url's and jQuery selectors.
    Because the mobile site of shelfari.com required the user to be logged
    in to search, the plugin now uses the main site search page to get the
    book URL.
    Const object was created to easily update and maintain url's and jQuery
    selectors.

--- main.js
+++ main.js
@@ -1,30 +1,42 @@
+var Const = {
+	URL      : 'http://www.shelfari.com/booksearch.aspx?keywords={isbn}',
+	QUERY    : '.book_results li:first h3 a',
+	TAG      : '{isbn}',
+	LOCATION : '.GFTButtonCondo'
+};
+
 (function prepareDocument() {
 	var store, isbn, a, img, href, mhref;
+	
 	store = $('input[name="storeId.att"]').val();
 	if (store !== 'books' && store !== 'digital-text') return;
 	
 	isbn = $('input[name="asin.att"]').val();
 	if (!isbn) return;
 	
-	href = 'http://www.shelfari.com/booksearch.aspx?keywords=' + isbn;
-	mhref = 'http://m.shelfari.com/search.aspx?k=' + isbn;
+	href  = Const.URL.replace(Const.TAG, isbn);
 		
-	a = $('<a></a>').attr('href', href).css({
-	   display: 'block',
-	   padding: 4,
-	   textAlign: 'center'
+	a = $('<a></a>').attr({
+		href     : href,
+		target   : '_blank'
+	}).css({
+	   display   : 'block',
+	   padding   : 4,
+	   textAlign : 'center'
     });
+    
 	img = $('<img />').attr('src', chrome.extension.getURL('button.png'));
 	img.appendTo(a);
 	
-	$('.GFTButtonCondo').append(a);
+	$(Const.LOCATION).append(a);
 	
 	$.ajax({
-	   url: mhref,
+	   url: href,
 	   type: 'GET',
+	   context: this,
 	   success: function(data) {
-	       var href = $('#SearchButton ~ b a', data).attr('href');
-	       if (href) a.attr('href', href.replace('/m.', '/www.'));
+	       var link = $(Const.QUERY, data).attr('href');
+	       if (link) a.attr('href', link);
 	   }
 	})
 })();
